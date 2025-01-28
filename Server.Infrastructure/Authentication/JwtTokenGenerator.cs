321eb14781267d67df4336d58feada7f15668623
pphatt <vutienphat0809@gmail.com>
2025-01-28 23:00:31 +0700

    feat: add jwt token generator

---
+++ Server.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Server.Application.Common.Interfaces.Authentication;
+using Server.Application.Common.Interfaces.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Server.Infrastructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider, JwtSettings jwtSettings)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _jwtSettings = jwtSettings;
+    }
+
+    public string GenerateToken(Guid UserId, string FirstName, string LastName)
+    {
+        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret)), SecurityAlgorithms.HmacSha256);
+
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, UserId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, FirstName.ToString()),
+            new Claim(JwtRegisteredClaimNames.FamilyName, LastName.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: _dateTimeProvider.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+            claims: claims,
+            signingCredentials: signingCredentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+}
--- Server.Infrastructure/DependencyInjection.cs
+++ Server.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Server.Application.Common.Interfaces;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Server.Application.Common.Interfaces.Authentication;
+using Server.Application.Common.Interfaces.Services;
+using Server.Infrastructure.Authentication;
 using Server.Infrastructure.Services;
 
 namespace Server.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SelectionName));
+
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddScoped<IDateTimeProvider, DateTimeProvider>();
 
         return services;
     }
